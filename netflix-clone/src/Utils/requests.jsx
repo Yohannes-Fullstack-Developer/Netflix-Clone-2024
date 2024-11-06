f80e2ff20b44ed6224430f28bd816a111971eb86
Yohannes Geberhanna <yohat7@gmail.com>
2024-11-06 09:52:34 -0500

    added Banner section

--- netflix-clone/src/Utils/requests.jsx
+++ netflix-clone/src/Utils/requests.jsx
@@ -1,6 +1,9 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+let API_KEY=import.meta.env.VITE_API_KEY
+// const TMDB_TOKEN =process.env.REACT_APP_TMDB_TOKEN
+0
 
-const request = {
+
+const requests = {
      fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
      fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
      fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -9,7 +12,7 @@ const request = {
      fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
      fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
      fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-     fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+     fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
 };
 
-export default request
+export default requests
