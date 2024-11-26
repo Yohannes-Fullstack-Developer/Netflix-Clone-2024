65fb0e6c3eb3483e1ae7c7b8454b1a007a57b66f
Yohannes Geberhanna <yohat7@gmail.com>
2024-11-26 10:08:52 -0500

    deploy

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./deploying-vite-Netflix-Clone-2024",
+  base: "/Netflix-Clone-2024/",
   plugins: [react()],
 })
